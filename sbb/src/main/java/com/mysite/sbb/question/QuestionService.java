597ffb67ab2947c2586a10f55ef8176623b3e0dc
chaji <chaj1684@gmail.com>
2025-11-05 02:56:43 +0900

    2-10 상세 페이지 만들기

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
+
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
